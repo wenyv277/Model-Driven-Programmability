adceffe4a6c1c7bb85a8260517002b836f9a1ab2
wenyv277 <34942795+wenyv277@users.noreply.github.com>
2021-10-28 11:02:03 +0200

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,10 @@
+firstName=input("What is your first name?")
+lastName=input("What is your last name ?")
+location=input("What is your location ?")
+age=input("What is your age ?")
+space=" "
+print("Hello"+space+firstName+space+lastName+space+"from"+space+location+space+"!")
+print("Hello"+space+firstName+space+lastName+space+"from"+space+location+space+"!"+space+"Your age is"+space+age+space+".")
+
+
+
